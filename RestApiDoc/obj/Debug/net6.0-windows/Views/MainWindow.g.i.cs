5c8ff28663b06b6255f6d8490d1316cf66947290
unknown <ulyanovskiy.01@mail.ru>
2022-04-12 13:33:56 +0300

    added setup

--- RestApiDoc/obj/Debug/net6.0-windows/Views/MainWindow.g.i.cs
+++ RestApiDoc/obj/Debug/net6.0-windows/Views/MainWindow.g.i.cs
@@ -79,7 +79,7 @@ namespace RestApiDoc {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RestApiDoc;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RestApiDoc;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
